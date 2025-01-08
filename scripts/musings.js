2e65257f42acae9e44965272c91fcccdf1bb8750
Edmond Wen <edmond@Edmond-Wen.local>
2025-01-08 17:06:49 -0500

    new musing post, new buttons

--- scripts/musings.js
+++ scripts/musings.js
@@ -1,9 +1,15 @@
+// create a class to manage state (which post is open at a time, date of posts to track most recent post update, etc.)
+
+function getAllPosts(){
+  // get list of all musing posts
+  return document.getElementById("musings").getElementsByClassName("musing-post");
+}
+
 function makePostsCollapsible(){
-    let posts = document.getElementById("musings");
-    let coll = posts.getElementsByClassName("musing-post");
+    let posts = getAllPosts();
     let i;
-    for (i = 0; i < coll.length; i++) {
-      coll[i].addEventListener("click", function() {
+    for (i = 0; i < posts.length; i++) {
+      posts[i].addEventListener("click", function() {
         content = this.getElementsByClassName("musing-content")[0];
         if (content.style.maxHeight){
           content.style.maxHeight = null;
@@ -26,15 +32,54 @@ function makeMusingPost(title, content){
     // create collapsible musing
     contentTag.style.transition = "max-height 0.2s ease-out";
 
-
     // update content
     titleTag.innerText = title;
     contentTag.innerText = content;
     root.appendChild(newPost);
 }
 
+function openFirstPost(){
+    // get the first post and uncollapse it using the "click" event
+    let posts = getAllPosts();
+    posts[0].click();
+}
+
+function closeAllPosts(){
+  let posts = getAllPosts();
+  for (let i = 0; i < posts.length; i++) {
+    content = posts[i].getElementsByClassName("musing-content")[0];
+    content.style.maxHeight = null;
+  }
+}
+
+function openRandomPost(){
+    // close all existing posts and open a random one.
+    // TODO: don't open a post that's already open
+    closeAllPosts();
+    let posts = getAllPosts();
+    let randomIndex = Math.floor(Math.random() * (posts.length - 1));
+    posts[randomIndex].click();
+}
+
+function openAllPosts(){
+  let posts = getAllPosts();
+  for (let i = 0; i < posts.length; i++) {
+    content = posts[i].getElementsByClassName("musing-content")[0];
+    content.style.maxHeight = "100%";
+  }
+}
+
+function addButtonEvents(){
+  const closeButton = document.getElementById("close-all-musing-button");
+  closeButton.addEventListener("click", closeAllPosts);
+
+  const openButton = document.getElementById("open-all-musing-button");
+  openButton.addEventListener("click", openAllPosts);
+
+  const randomButton = document.getElementById("random-musing-button");
+  randomButton.addEventListener("click", openRandomPost);
+}
+
 makePostsCollapsible();
-
-// makeMusingPost("november 7th / post election blues, mom visit", "lol");
-// makeMusingPost("november 1st / this is a musing", "this is my first musing on this page. it is currently 5:34 pm at the office. I've been working on this little personal website for the past ~1.5 hrs. I finished my fde related task earlier this morning but I'm not sure what my next steps are so I decided to work on this instead. Raymond, Boyu, and Saurav are still in the office. I am killing time until Alan's birthday dinner at 7pm. There was jazz playing in the meatpacking plaza (idk its official name) earlier. I dropped off my ballot today at the nearby post office. It is now 5:39 pm. What should I get Alan? This site is public so he could be reading this 😳. Hi Alan! Happy birthday! I think I'll take a walk. \n \n I went to Chong's Pleasure Palace last night to watch Incantation. The F train home at 10:30pm last night was packed like a can of sardines. As of tomorrow, I will have been in NYC for 3 months. I've experienced ridiculous emotional swings. Some days I feel devastatingly lonely. On others, I feel exhilarated. Today, I feel alright.");
-
+// openFirstPost();
+addButtonEvents();
